0a3f0d1016d76f7680857d8af786dd5792f025b0
Patrik Uytterhoeven <patrik.uytterhoeven@gmail.com>
2026-03-01 08:59:09 +0100

    Render only hosts, global row sorting and datasets in edit form

--- views/widget.edit.php
+++ views/widget.edit.php
@@ -7,17 +7,7 @@ declare(strict_types = 1);
 $form = new CWidgetFormView($data);
 
 $form->addField(new CWidgetFieldMultiSelectHostView($data['fields']['hostids']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['item_key_search']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['item_name_search']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['lookback_hours']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['max_rows']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['history_points']));
-$form->addField(new CWidgetFieldSelectView($data['fields']['merge_equal_states']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['merge_shorter_than']));
-$form->addField(new CWidgetFieldSelectView($data['fields']['null_gap_mode']));
-$form->addField(new CWidgetFieldSelectView($data['fields']['null_gap_backfill_first']));
 $form->addField(new CWidgetFieldSelectView($data['fields']['row_sort']));
-$form->addField(new CWidgetFieldTextBoxView($data['fields']['state_map']));
 $form->addField(new CWidgetFieldTextBoxView($data['fields']['datasets_json']));
 
 $widget_edit_js = file_get_contents(__DIR__.'/../assets/js/widget.edit.js');
