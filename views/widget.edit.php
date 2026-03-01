85e587a03aa83eeda32ae8cbc0bf38fc31983991
Patrik Uytterhoeven <patrik.uytterhoeven@gmail.com>
2026-03-01 18:29:58 +0100

    fix(edit): show legend and segment label options in form

--- views/widget.edit.php
+++ views/widget.edit.php
@@ -8,6 +8,10 @@ $form = new CWidgetFormView($data);
 
 $form->addField(new CWidgetFieldMultiSelectHostView($data['fields']['hostids']));
 $form->addField(new CWidgetFieldSelectView($data['fields']['row_sort']));
+$form->addField(new CWidgetFieldSelectView($data['fields']['legend_mode']));
+$form->addField(new CWidgetFieldSelectView($data['fields']['legend_show_count']));
+$form->addField(new CWidgetFieldSelectView($data['fields']['legend_show_duration']));
+$form->addField(new CWidgetFieldSelectView($data['fields']['segment_label_mode']));
 $form->addField(new CWidgetFieldTextBoxView($data['fields']['datasets_json']));
 
 $widget_edit_js = file_get_contents(__DIR__.'/../assets/js/widget.edit.js');
